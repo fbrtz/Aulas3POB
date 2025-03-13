c2212dc110aac1b8e9ab108bf39dc74407c0a50d
fbrtz <113561869+fbrtz@users.noreply.github.com>
2025-03-13 12:06:24 -0300

    Update ConverteNota.java
    Adição da condicional para interromper a execução caso a nota não esteja no intervalo indicado.

--- exercicios2/ex5/ConverteNota.java
+++ exercicios2/ex5/ConverteNota.java
@@ -9,33 +9,36 @@ public class ConverteNota {
             System.out.print("Digite a nota do aluno: ");
             float nota = sc.nextFloat();
             char conceito;
-
-            switch ((int)nota) {
-                case 9:
-                case 10:
-                    conceito = 'A';
-                    break;
-                case 7:
-                case 8:
-                    conceito = 'B';
-                    break;
-                case 5:
-                case 6:
-                    conceito = 'C';
-                    break;
-                case 3:
-                case 4:
-                    conceito = 'D';
-                    break;
-                case 0:
-                case 1:
-                case 2:
-                    conceito = 'E';
-                    break;
-                default:
-                    conceito = ' ';
-                    System.out.println("Nota inválida.");
-                    break;
+            if (nota<0 || nota>10){
+              System.out.println("Nota inválida");
+            }else{
+              switch ((int)nota) {
+                  case 9:
+                  case 10:
+                      conceito = 'A';
+                      break;
+                  case 7:
+                  case 8:
+                      conceito = 'B';
+                      break;
+                  case 5:
+                  case 6:
+                      conceito = 'C';
+                      break;
+                  case 3:
+                  case 4:
+                      conceito = 'D';
+                      break;
+                  case 0:
+                  case 1:
+                  case 2:
+                      conceito = 'E';
+                      break;
+                  default:
+                      conceito = ' ';
+                      System.out.println("Nota inválida.");
+                      break;
+              }
             }
             System.out.println("O conceito do aluno é: "+conceito);
 
